e47ed1da78ff2b6d177203ca4480fabd4c211d73
Hamza <hamzak777@gmail.com>
2021-08-06 23:52:23 -0400

    Update assignment_3.blade.php
    Added CSS

--- assignment_3/resources/views/assignment_3.blade.php
+++ assignment_3/resources/views/assignment_3.blade.php
@@ -5,6 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equive="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <style>
+    h1 {
+    color: black;
+    margin-left: 40px;
+    font-size: 40px;
+    }
+    h2 {
+    color: black;
+    margin-left: 40px;
+    font-size: 35px;
+    }
+    body {
+	background-color: #E6E6FA;
+	color: green;
+    }
+    p {
+        color: #685206; 
+        font-family: Helvetica Neue, sans-serif;
+        font-size: 28px;
+        margin-left: 40px;
+        margin-right: 40px;
+    }
+    </style>
 </head>
 <body>
     <h1>{{ $title }}</h1>
